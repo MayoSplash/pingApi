656d0735ad5abaacd6c028211ddcc0b2b7af757c
MayoSplash <roman.stepanenko.01@gmail.com>
2022-07-18 13:51:57 +0300

    so so

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import platform
+import subprocess
+from pythonping import ping
 
+app = Flask(__name__)
+api = Api(prefix="/api")
 
+class Ping(Resource):
+    def get(self, ip):
+        parameter = '-n' if platform.system().lower() == 'windows' else '-c'
 
+        command = ['ping', parameter, '1', ip]
+        response = subprocess.call(command)
+
+        if response == 0:
+            return True
+        else:
+            return False
+
+api.add_resource(Ping, "/<ip>")
+api.init_app(app)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=30)
