9aeb18372fb58afcc981f3a9ee69a8f09c429762
MayoSplash <roman.stepanenko.01@gmail.com>
2022-07-21 14:21:49 +0300

    Реализовал авторизацию

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 from flask import Flask
 from flask_restful import request
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 import platform
 import subprocess
-import json
+
+users = {
+    "john": generate_password_hash("hello")
+}
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
 
 @app.route('/api/json', methods=['POST'])
-def json():
+@auth.login_required
+def post_query_result():
     data = request.get_json()
-
     result = {}
     for ip in data["ip"]:
         parameter = '-n' if platform.system().lower() == 'windows' else '-c'
         command = ['ping', parameter, '1', ip]
         response = subprocess.call(command) == 0
         result[ip] = response
-
     return result
 
 @app.route('/api/check/<ip>', methods=['GET'])
-def check(ip):
+def get_query_result(ip):
     result = {}
     parameter = '-n' if platform.system().lower() == 'windows' else '-c'
     command = ['ping', parameter, '1', ip]
     response = subprocess.call(command) == 0
     result[ip] = response
     return result
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=30)
--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import requests
 import json
 
+
+input = open("input.txt", mode="r")
+lines = []
+for line in input:
+    lines.append(line)
+input.close()
+lines = [line.rstrip() for line in lines]
+
 url = "http://127.0.0.1:30/api/json"
 headers = {"Content-type": "application/json"}
 data = json.dumps({
-    "ip": ["8.8.8.8", "127.0.0.1:30", "55.55.22.12"]
+    "ip": lines
 })
-
 
 req = requests.post(url=url, headers=headers, data=data)
 
-print(req.text)
+with open("output.txt", "w") as output:
+    output.writelines(str(req.text))
