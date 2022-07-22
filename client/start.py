f32cbf1e8bec54572ba464fd170cc45d4157c8cf
MayoSplash <roman.stepanenko.01@gmail.com>
2022-07-22 15:41:46 +0300

    Все работает

--- client/start.py
+++ client/start.py
@@ -1,6 +1,7 @@
+import csv
+
 import requests
 import json
-
 
 input = open("input.txt", mode="r")
 lines = []
@@ -8,7 +9,6 @@ for line in input:
     lines.append(line)
 input.close()
 lines = [line.rstrip() for line in lines]
-
 
 username = 'john'
 password = 'hello'
@@ -18,7 +18,20 @@ data = json.dumps({
     "ip": lines
 })
 
-req = requests.post(url=url, headers=headers, data=data, auth=(username, password))
-
-with open("output.json", "w+") as output:
+req = (requests.post(url=url, headers=headers, data=data, auth=(username, password)))
+with open("output.txt", "w+") as output:
     output.writelines(str(req.text))
+    output.close()
+
+
+output = open("output.csv", "w+")
+data = json.loads(req.text)
+for key in data:
+    output.write(key + ";" + str(data[key]) + "\r")
+
+output.close()
+
+
+
+
+
